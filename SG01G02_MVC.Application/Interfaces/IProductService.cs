38d2e03218c515937418c7e4c71b386ad98d81a1
Niklas H <156240330+mymh13@users.noreply.github.com>
2025-04-13 23:28:45 +0200

    SCRUM-45: Feature - Created IProductService interface in Application layer

--- SG01G02_MVC.Application/Interfaces/IProductService.cs
+++ SG01G02_MVC.Application/Interfaces/IProductService.cs
@@ -1,7 +1,10 @@
+using SG01G02_MVC.Domain.Entities;
+
 namespace SG01G02_MVC.Application.Interfaces
 {
     public interface IProductService
     {
-        // TODO: Implement the ProductService interface
+        IEnumerable<Product> GetAllProducts();
+        Product? GetProductById(int id);
     }
 }
