7b8128b94406b3760f02c22e21c22faf0ca1c7d3
Niklas H <156240330+mymh13@users.noreply.github.com>
2025-04-29 14:49:59 +0200

    SCRUM-174: Enabled EF Core migrations and refactored repository/test layers to match async interface

--- SG01G02_MVC.Application/Interfaces/IProductService.cs
+++ SG01G02_MVC.Application/Interfaces/IProductService.cs
@@ -4,8 +4,8 @@ namespace SG01G02_MVC.Application.Interfaces
 {
     public interface IProductService
     {
-        IEnumerable<ProductDto> GetAllProducts();
-        ProductDto? GetProductById(int id);
+        Task<IEnumerable<ProductDto>> GetAllProducts();
+        Task<ProductDto?> GetProductById(int id);
         Task CreateProductAsync(ProductDto dto);
         Task UpdateProductAsync(ProductDto dto);
         Task DeleteProductAsync(int id);
--- SG01G02_MVC.Application/Services/ProductService.cs
+++ SG01G02_MVC.Application/Services/ProductService.cs
@@ -13,15 +13,15 @@ namespace SG01G02_MVC.Application.Services
             _repository = repository;
         }
 
-        public IEnumerable<ProductDto> GetAllProducts()
+        public async Task<IEnumerable<ProductDto>> GetAllProducts()
         {
-            var products = _repository.GetAllProducts();
+            var products = await _repository.GetAllProducts();
             return products.Select(MapToDto);
         }
 
-        public ProductDto? GetProductById(int id)
+        public async Task<ProductDto?> GetProductById(int id)
         {
-            var product = _repository.GetProductById(id);
+            var product = await _repository.GetProductById(id);
             return product == null ? null : MapToDto(product);
         }
 
