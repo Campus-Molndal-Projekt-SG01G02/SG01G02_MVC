45b05488d40b707d2065fa8ac5dc083be244aa5c
sirhaffy <haffy@icloud.com>
2025-05-09 16:54:46 +0200

    Removing errors.

--- SG01G02_MVC.Infrastructure/External/ReviewApiClient.cs
+++ SG01G02_MVC.Infrastructure/External/ReviewApiClient.cs
@@ -2,22 +2,24 @@ using SG01G02_MVC.Application.DTOs;
 using SG01G02_MVC.Application.Interfaces;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Net.Http.Json;
 
 namespace SG01G02_MVC.Infrastructure.External;
 
 public class ReviewApiClient : IReviewApiClient
 {
-    // TODO: Implement Review API client
-    
-    public async Task<IEnumerable<ReviewDto>> GetReviewsAsync(string productId)
+    private readonly HttpClient _httpClient;
+
+    public ReviewApiClient(HttpClient httpClient)
     {
-        // Implementation
-        // return new List<ReviewDto>(); // Placeholder return value
-        throw new NotImplementedException();
+        _httpClient = httpClient;
     }
 
-    public Task<IEnumerable<ReviewDto>> GetReviewsForProductAsync(string productId)
+    public async Task<IEnumerable<ReviewDto>> GetReviewsAsync(string productId)
     {
-        throw new NotImplementedException();
+        // TODO Fix this..
+        await Task.Delay(1); 
+        return new List<ReviewDto>(); 
+
     }
 }
