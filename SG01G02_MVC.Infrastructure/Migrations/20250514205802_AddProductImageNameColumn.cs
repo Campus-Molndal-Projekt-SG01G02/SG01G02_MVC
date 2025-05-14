9c057e12b64a340aa02bc08c1a192b5df01d4401
sirhaffy <haffy@icloud.com>
2025-05-14 23:00:29 +0200

    EF Migration fix.

---
+++ SG01G02_MVC.Infrastructure/Migrations/20250514205802_AddProductImageNameColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SG01G02_MVC.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductImageNameColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "Products",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "Products");
+        }
+    }
+}
